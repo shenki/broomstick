e51dbee183c955c5dab46b83f331a319cb7cc3a9
Joel Stanley <joel@jms.id.au>
2016-04-08 15:11:27 +1000

    pdbg: Update version to fix SBE cranking
    This enables us to power on using pdbg.
    
    Signed-off-by: Joel Stanley <joel@jms.id.au>

--- openbmc/package/pdbg/pdbg.mk
+++ openbmc/package/pdbg/pdbg.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PDBG_VERSION = 8af274e57c509807634228c8089ad4001b639925
+PDBG_VERSION = 5879afc52971c5785792867dcd85dfc771fad274
 
 PDBG_SITE = $(call github,open-power,pdbg,$(PDBG_VERSION))
 PDBG_LICENSE = Apache 2.0
