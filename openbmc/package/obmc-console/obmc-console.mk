32679e891368e22be305bc786faa4a853d566fe5
Joel Stanley <joel@jms.id.au>
2016-09-01 00:06:54 +0930

    openbmc/obmc-console: Update to latest
    Adds poll.h patch required for building with musl.
    
    Signed-off-by: Joel Stanley <joel@jms.id.au>

--- openbmc/package/obmc-console/obmc-console.mk
+++ openbmc/package/obmc-console/obmc-console.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OBMC_CONSOLE_VERSION = 54e9569d14b127806e45e1c17ec4a1f5f7271d3f
+OBMC_CONSOLE_VERSION = 87e344cd6bd848f886e226c8d58ffe4da77ce4bc
 
 OBMC_CONSOLE_SITE = $(call github,openbmc,obmc-console,$(OBMC_CONSOLE_VERSION))
 OBMC_CONSOLE_LICENSE = Apache 2.0
