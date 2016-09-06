08a128cf52db05187b8551db86c93b92e0bf759f
Joel Stanley <joel@jms.id.au>
2016-09-06 13:04:43 +0930

    boot.sh: Add names of registers
    Signed-off-by: Joel Stanley <joel@jms.id.au>

--- openbmc/overlay/usr/bin/boot.sh
+++ openbmc/overlay/usr/bin/boot.sh
@@ -50,26 +50,30 @@ echo 0 > /sys/class/gpio/gpio${PCIE_RST_N}/value
 echo 0 > /sys/class/gpio/gpio${PEX_PERST_N}/value
 sleep 10
 
+LPC_SCR0SIO=0x1e789170
+LPC_SCR1SIO=0x1e789174
+LPC_HCR5=0x1e789080
+LPC_HCR7=0x1e789088
+LPC_HCR8=0x1e78908C
+
 # LPC HCR5
 #  10: Enable LPC FHW cycles
 #   8: Enable LPC to AHB bridge
-devmem 0x1e789080 32 0x00000500
+devmem $(LPC_HCR5) 32 0x00000500
 
 # LPC HCR7: LPC to AHB bridge
 #  ADRBASE: remapping base address
 #  HWMBASE: decoding base address [31:16]
-devmem 0x1e789088 32 0x30000E00
+devmem $(LPC_HCR7) 32 0x30000E00
 
 # LPC HICR8: LPC to AHB bridge
 #  ADDRMASK: remapping mask
-#   HWNCARE: adecoding range control bit
-devmem 0x1e78908C 32 0xFE0001FF
+#   HWNCARE: decoding range control bit
+devmem $(LPC_HCR8) 32 0xFE0001FF
 
-# LPC SCR0SIO
-devmem 0x1e789170 32 0x00000042
-
-# LPC SCR1SIO
-devmem 0x1e789174 32 0x00004000
+# Scratch registers
+devmem $(LPC_SCR0SIO) 32 0x00000042
+devmem $(LPC_SCR1SIO) 32 0x00004000
 
 # Make sure flash is functional
 pflash -i
